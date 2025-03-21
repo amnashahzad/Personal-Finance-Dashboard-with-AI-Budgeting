5cd7e3f4999a13b43d9c3eed7370d9799098a964
Amna Shahzad <121329321+amnashahzad@users.noreply.github.com>
2025-03-21 07:33:06 +0500

    Update personal.py

--- personal.py
+++ personal.py
@@ -8,11 +8,11 @@ from sklearn.pipeline import make_pipeline
 from sklearn.metrics import classification_report
 
 def load_budget():
-    budget = pd.read_csv(r'/workspaces/Personal-Finance-Dashboard-with-AI-Budgeting/Budget.csv')
+    budget = pd.read_csv(r'Budget.csv')
     return budget
 
 def load_transactions():
-    transactions = pd.read_csv(r'/workspaces/Personal-Finance-Dashboard-with-AI-Budgeting/personal_transactions.csv')
+    transactions = pd.read_csv(r'personal_transactions.csv')
     transactions['Date'] = pd.to_datetime(transactions['Date'], format='%m/%d/%Y')
     return transactions
 
